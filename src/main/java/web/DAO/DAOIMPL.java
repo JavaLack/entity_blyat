4b01fcfdfad5dc6429ac0282c22845ed82cac5e1
JavaLack <120420479+JavaLack@users.noreply.github.com>
2023-09-01 12:56:43 +0300

    Update DAOIMPL.java

--- src/main/java/web/DAO/DAOIMPL.java
+++ src/main/java/web/DAO/DAOIMPL.java
@@ -25,8 +25,4 @@ public class DAOIMPL implements DAO {
         return cars.stream().limit(count).toList();
     }
 
-//    @Override
-//    public List<Car> getOllCars() {
-//        return cars;
-//    }
 }
